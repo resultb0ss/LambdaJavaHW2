074ea835c8927fa865ab071ab07b18b7804e099e
Admin <reteamboss@gmail.com>
2024-12-04 14:21:37 +0300

    3

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] array = {"Виктор", "Андрей", "Великослав", "Джамаллитдин", "Великобритания", "Франция"};
+
+        Expression exp = (a, b) -> a > b ;
+        System.out.println(longestWord(array, exp));
+
+
+    }
+
+    public static String longestWord(String[] array, Expression exp) {
+        String result = array[0];
+        for (String element : array) {
+            if (exp.isEqual(element.length(), result.length())) {
+                result = element;
+            }
+        }
+        return result;
+    }
+
+}
+
+interface Expression {
+    boolean isEqual(int a, int b);
+}
